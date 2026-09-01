7a964b9fa84424c0dd1fb2b628addf3ab1d43d48
tzschmidt <tzschmidt@gmx.de>
2026-09-01 15:38:37 +0200

    Move argument parsing to extra parser

--- src/setselector/__main__.py
+++ src/setselector/__main__.py
@@ -4,6 +4,7 @@ The main entry point for the application.
 
 import sys
 
+from .selector import Selector
 from .utils.logging import configure_logging, get_logger
 from .utils.parser import get_parser
 
@@ -17,10 +18,28 @@ def main() -> None:
     configure_logging(sys.stderr, args.log, sys.stderr.isatty())
 
     log = get_logger("main")
-    log.info("info")
-    log.warning("warning")
-    log.debug("debug")
-    log.error("error")
+
+    if 0 < args.easyK * args.cutoff < 10.0:
+        args.easyK = 10.0 / args.cutoff
+
+    selector = Selector(args.cutoff)
+    selector.parse_features(args.feats)
+    selector.parse_runtimes(args.times)
+
+    if args.split:
+        selector.random_test_training_split()
+
+    selector.join_times_features()
+    selector.runtime_of_samples(list(selector._runtime_data_dic.keys()))
+    selector.remove_too_easy(args.cutoff, args.easyK)
+    selector.clustering(args.reps)
+    samples = selector.select(args.n, args.frac, args.agg, args.dist)
+    selector.print_samples(samples)
+    selector.runtime_of_samples(samples)
+    selector.features_of_samples(samples)
+    selector.print_stats()
+
+    log.debug("done")
 
 
 if __name__ == "__main__":
--- src/setselector/utils/parser.py
+++ src/setselector/utils/parser.py
@@ -50,4 +50,49 @@ def get_parser() -> ArgumentParser:
     )
 
     parser.add_argument("--version", "-v", action="version", version=f"%(prog)s {VERSION}")
+
+    req_group = parser.add_argument_group("Required Options")
+    req_group.add_argument(
+        "--runtimes", dest="times", action="store", required=True, help="runtimes in csv (first col with instance names"
+    )
+    req_group.add_argument(
+        "--features", dest="feats", action="store", required=True, help="instance features in csv (first col with instance names"
+    )
+    req_group.add_argument("--cutoff", dest="cutoff", action="store", type=int, required=True, help="cutoff time")
+    req_group.add_argument("--n", dest="n", action="store", type=int, required=True, help="desired number of instances")
+
+    opt_group = parser.add_argument_group("Optional Options")
+    opt_group.add_argument(
+        "--reps", dest="reps", action="store", default=100, type=int, help="repetitions of kmeans clustering"
+    )
+    opt_group.add_argument(
+        "--frac", dest="frac", action="store", default=0.2, type=float, help="maximum representation of each cluster [0,1]"
+    )
+    opt_group.add_argument(
+        "--easyK",
+        dest="easyK",
+        action="store",
+        default=0.1,
+        type=float,
+        help="remove too easy instances (solved by all solvers and avg runtimes < k*cutoff)",
+    )
+    opt_group.add_argument(
+        "--aggregate",
+        dest="agg",
+        action="store",
+        default="avg",
+        choices=["avg", "min", "ind"],
+        help="aggregation of instance runtimes",
+    )
+    opt_group.add_argument(
+        "--dist", dest="dist", action="store", default="gauss", choices=["gauss", "uni", "exp", "log"], help="sample distribution"
+    )
+    opt_group.add_argument(
+        "--split",
+        dest="split",
+        action="store_true",
+        default=False,
+        help="before sampling, split randomly instance set in test and training set",
+    )
+
     return parser
