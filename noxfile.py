ab4c2575014fb85097bc8e7d60fb15f18e5e1e15
tzschmidt <tzschmidt@gmx.de>
2026-09-03 15:47:22 +0200

    Add formatter

--- noxfile.py
+++ noxfile.py
@@ -6,12 +6,51 @@ import os
 
 import nox  # type: ignore
 
-nox.options.sessions = "lint", "typecheck", "test"
+nox.options.sessions = "format, lint", "typecheck", "test"
 nox.options.default_venv_backend = "uv|virtualenv"
 
 PYTHON_VERSIONS = None
 if "GITHUB_ACTIONS" in os.environ:
-    PYTHON_VERSIONS = ["3.11", "3.14"]
+    PYTHON_VERSIONS = ["3.12", "3.14"]
+
+FILES_TO_BE_CHECKED = [
+    "src",
+]
+
+
+@nox.session
+def format(session):
+    """
+    Autoformat source files.
+
+    If argument check is given, only reports changes.
+    """
+    session.install("-e", ".[format]")
+    check = "check" in session.posargs
+
+    autoflake_args = [
+        "--in-place",
+        "--imports=benchmarktool",
+        "--ignore-init-module-imports",
+        "--remove-unused-variables",
+        "-r",
+        "tests",
+    ] + FILES_TO_BE_CHECKED
+    if check:
+        autoflake_args.remove("--in-place")
+    session.run("autoflake", *autoflake_args)
+
+    isort_args = ["--profile", "black", "tests"] + FILES_TO_BE_CHECKED
+    if check:
+        isort_args.insert(0, "--check")
+        isort_args.insert(1, "--diff")
+    session.run("isort", *isort_args)
+
+    black_args = ["tests"] + FILES_TO_BE_CHECKED
+    if check:
+        black_args.insert(0, "--check")
+        black_args.insert(1, "--diff")
+    session.run("black", *black_args)
 
 
 @nox.session
@@ -42,7 +81,7 @@ def typecheck(session):
     """
     if not session.virtualenv._reused:
         session.install(".[typecheck]")
-    session.run("ty", "check")
+    session.run("ty", "check", *FILES_TO_BE_CHECKED)
 
 
 @nox.session(python=PYTHON_VERSIONS)
